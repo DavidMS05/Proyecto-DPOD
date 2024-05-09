d92290feb516abc504f1d856fd66b8afe51efd4e
BlueZeroV <azerosb@gmail.com>
2024-05-10 00:15:04 +0200

    Avance

--- codigo.js
+++ codigo.js
@@ -1,3 +1,8 @@
+function iniciarEncuesta() {
+    document.getElementById("preguntas").style.display = "block";
+    document.getElementById("iniciarCuestionario").style.display = "none";
+}
+
 function calcularPuntuacion() {
     var pregunta1 = parseInt(document.getElementById("pregunta1").value);
     var pregunta2 = parseInt(document.getElementById("pregunta2").value);
@@ -12,11 +17,10 @@ function calcularPuntuacion() {
 
     var puntuacionTotal = pregunta1 + pregunta2 + pregunta3 + pregunta4 + pregunta5 + pregunta6 + pregunta7 + pregunta8 + pregunta9 + pregunta10;
 
-    document.getElementById("resultado").innerHTML= "<p>Tu puntuacion es "+ puntuacionTotal + "</p>";
-
     if(pregunta1 == 0 || pregunta2 == 0 || pregunta3 == 0 || pregunta4 == 0 || pregunta5 == 0 || pregunta6 == 0 || pregunta7 == 0 || pregunta8 == 0 || pregunta9 == 0 || pregunta10 == 0){
         alert("Por favor responde a todas las preguntas")
     } else{
+        document.getElementById("resultado").innerHTML= "<span>Tu puntuacion es "+ puntuacionTotal + "</span>";
         señalarfila(puntuacionTotal)
     }
 }
@@ -29,12 +33,16 @@ function señalarfila(puntuacionTotal){
 
     if(puntuacionTotal >= 350 && puntuacionTotal <= 400){
         document.getElementById("400p").style.backgroundColor = " #8ac43f";
+        document.getElementById("contenido-grado-cuatro").style.display = "block";
     }else if(puntuacionTotal >= 250 && puntuacionTotal <= 350){
         document.getElementById("300p").style.backgroundColor = " #8ac43f";
+        document.getElementById("contenido-grado-tres").style.display = "block";
     }else if(puntuacionTotal >= 150 && puntuacionTotal <= 250){
         document.getElementById("200p").style.backgroundColor = " #8ac43f";
+        document.getElementById("contenido-grado-dos").style.display = "block";
     }else if(puntuacionTotal >= 50 && puntuacionTotal <= 150){
         document.getElementById("100p").style.backgroundColor = " #8ac43f";
+        document.getElementById("contenido-grado-uno").style.display = "block";
     }
 
     var spans = document.getElementsByClassName("span");
@@ -42,7 +50,3 @@ function señalarfila(puntuacionTotal){
         spans[i].style.display = "block";
     }
 }
-function iniciarEncuesta() {
-    document.getElementById("preguntas").style.display = "block";
-    document.getElementById("iniciarCuestionario").style.display = "none";
-  }
